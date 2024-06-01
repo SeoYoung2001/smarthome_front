9637b57cd53e5d436122ee0f1b06a2aca1ccdf24
Seoyoung Park <ghok111@naver.com>
2024-06-01 17:57:21 +0900

    comittest

--- app/src/main/java/com/example/smarthomeapp/MainActivity.java
+++ app/src/main/java/com/example/smarthomeapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.smarthomeapp;
 import android.os.Bundle;
+import android.content.SharedPreferences;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.content.Context;
 import android.widget.RadioButton;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
@@ -27,8 +29,12 @@ public class MainActivity extends AppCompatActivity {
     Button button6;
     ImageButton button8;
     TextView textView;
+    TextView textView2;
+    SharedPreferences sharedPref;
 
     private static final String KEY_TEMPERATURE = "temperature";
+    private static final String KEY_HUMIDITY = "humidity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,36 +46,67 @@ public class MainActivity extends AppCompatActivity {
         button3 = findViewById(R.id.Light_Btn);
         button4 = findViewById(R.id.Sensor_Btn);
         button6 = findViewById(R.id.Lcd_Btn);
-        button8=findViewById(R.id.wifiBtn);
-
+        button8 = findViewById(R.id.wifiBtn);
         textView = findViewById(R.id.MainTemp_txt);
+        textView2 = findViewById(R.id.MainHud_txt);
+
         if (savedInstanceState != null) {
-            // Restore the temperature if available
+            // Restore the temperature and humidity if available
             String savedTemperature = savedInstanceState.getString(KEY_TEMPERATURE);
+            String savedHumidity = savedInstanceState.getString(KEY_HUMIDITY);
             if (savedTemperature != null) {
                 textView.setText(savedTemperature);
+            }
+            if (savedHumidity != null) {
+                textView2.setText(savedHumidity);
             }
         } else {
             // Get intent and retrieve data
             Intent i = getIntent();
             String temperature = i.getStringExtra("temperature");
+            String humidity = i.getStringExtra("humidity");
 
             // Check for null values and set text
             if (temperature != null) {
                 textView.setText(temperature);
             } else {
-                textView.setText(" ");
+                textView.setText("온도");
+            }
+
+            if (humidity != null) {
+                textView2.setText(humidity);
+            } else {
+                textView2.setText("습도");
             }
         }
 
-        // Set up OnClickListeners using a common method
         setupButtonListeners();
+
     }
+
+
+
+
+
+    @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        // Save the temperature
         outState.putString(KEY_TEMPERATURE, textView.getText().toString());
+        outState.putString(KEY_HUMIDITY, textView2.getText().toString());
     }
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // Restore the temperature and humidity if available
+        String savedTemperature = savedInstanceState.getString(KEY_TEMPERATURE);
+        String savedHumidity = savedInstanceState.getString(KEY_HUMIDITY);
+        if (savedTemperature != null) {
+            textView.setText(savedTemperature);
+        }
+        if (savedHumidity != null) {
+            textView2.setText(savedHumidity);
+        }
+    }
+
 
 
 
@@ -81,6 +118,8 @@ public class MainActivity extends AppCompatActivity {
         button6.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, LcdActivity.class)));
         button8.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, WifiActivity.class)));
     }
+
+
 
 
 
